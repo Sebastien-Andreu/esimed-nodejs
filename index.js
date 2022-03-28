cb4410b13ecc4a806052fb746aa783aa64822010
Sebastien-Andreu <sebastien.andreu@fiducial.net>
2022-03-28 08:47:07 +0200

    add tp1 correction

--- index.js
+++ index.js
@@ -1,41 +1,4 @@
-const Users = require('./routes/Users')
-const Express = require('express')
+const WebServer = require('./src/core/web-server');
 
-const port = 3000
-const app = Express()
-
-app.use(Express.json())
-
-app.get('/Users', (req, res) => {
-    Users.getAllUsers().then(r => {
-        res.send(r)
-    })
-})
-
-app.get('/User/:firstName', (req, res) => {
-    Users.getUserByFirstName(req.params.firstName).then(r => {
-        res.status(r.status).send(r.message)
-    })
-})
-
-app.post('/User',(req, res) => {
-    Users.add(req).then(r => {
-        res.status(r.status).send(r.message)
-    })
-})
-
-app.delete('/User/:id', (req, res) => {
-    Users.remove(req).then(r => {
-        res.status(r.status).send(r.message)
-    })
-})
-
-app.post('/User/:id', (req, res) => {
-    Users.update(req).then(r => {
-        res.status(r.status).send(r.message)
-    })
-})
-
-app.listen(port, () => {
-    console.log(`Server listen on port ${port}`)
-})
+const webServer = new WebServer();
+webServer.start();
